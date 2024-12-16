d3863956d96ea148e6c0f6766710eb37e3fb0548
fchimbera <chimberafreeman@gmail.com>
2024-12-16 21:26:27 +0200

    added pattern_drawing.py

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,10 @@
 pattern_size =int(input("Enter the size of the pattern: "))
 #added nested for loops to create pattern
-for x in range(1, pattern_size + 1):
-    for y in range(1, pattern_size + 1):
+x = 1
+while x <= pattern_size:
+    y = 1
+    while y <= pattern_size:
         print("*",end="")
+        y += 1
     print("")
+    x += 1
