1afe59a476057d4fa8f7f2412e9b9e6b58279626
fchimbera <chimberafreeman@gmail.com>
2024-12-16 11:43:55 +0200

    added multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
-number = input("Enter a number to see it multiplication table: ")
+number = int(input("Enter a number to see it multiplication table: "))
 #add loop to iterate through through numbers 1 to 10
 for y in range(1,11):
-    print(f"{number} * {y} = {int(number) * y}")
+    print(f"{number} * {y} = {number * y}")
