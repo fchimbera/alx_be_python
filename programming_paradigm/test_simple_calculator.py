5822122bf805b84820e574ddda0bf298bf9db484
fchimbera <chimberafreeman@gmail.com>
2025-01-15 22:04:21 +0200

    updated test simple calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -21,7 +21,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(-1, -2), 1)
         self.assertAlmostEqual(self.calc.subtract(10000.0, 9999.9), 0.1)
         
-    def test_multiply(self):
+    def test_multiplication(self):
         #test the multiply method
         self.assertEqual(self.calc.multiply(0, 0), 0)
         self.assertEqual(self.calc.multiply(0, 2), 0)
@@ -29,7 +29,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.multiply(-1, -1), 1)
         self.assertEqual(self.calc.multiply(2.5, 4), 10.0)
         
-    def test_divide(self):
+    def test_division(self):
         #test the divide method
         self.assertEqual(self.calc.divide(4, 2), 2)
         self.assertEqual(self.calc.divide(-4, -2), 2)
